04ac3038db172f74e71bee31f9dec5afe7844c7a
Cristian Chavez <22006761@galileo.edu>
2023-11-26 19:33:21 -0600

    Primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';  // Agrega esta línea para importar FormsModule
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, FormsModule], // Agrega FormsModule aquí
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  cantidadEnQuetzales: number = 0; // Inicializa con un valor predeterminado
+  resultado: number | undefined; // Marca como opcional usando `undefined`
 
   constructor() {}
 
+  convertirDivisa() {
+    // Utilizamos la tasa de cambio de 8 quetzales por dólar
+    const tasaCambio = 8;
+    this.resultado = this.cantidadEnQuetzales / tasaCambio;
+  }
 }
